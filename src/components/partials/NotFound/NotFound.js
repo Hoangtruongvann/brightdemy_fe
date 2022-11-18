c7c228ab70458c538f2f56070111b36c4da265bc
Hoàng Trương <hoangtruongvann@gmail.com>
2022-11-18 23:24:06 +0700

    404 not found

---
+++ src/components/partials/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="notfound">
+      <img alt="404 not found" src=".././HTML-404.png" />
+    </div>
+  );
+};
+export default NotFound;
